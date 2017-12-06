0070e1cb8e22941af689cbd180062e3601c3368f
nj <nj@example.com>
2017-12-06 17:32:32 +0800

    加入创建数据库和表的各项配置。

--- app/src/main/java/com/example/administrator/coolweather/MainActivity.java
+++ app/src/main/java/com/example/administrator/coolweather/MainActivity.java
@@ -1,7 +1,8 @@
-package android.coolweather.com.coolweather;
+package com.example.administrator.coolweather;
 
-import android.support.v7.app.AppCompatActivity;
+import android.coolweather.com.coolweather.R;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
