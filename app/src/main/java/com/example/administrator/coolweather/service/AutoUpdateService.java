e886b4803396b1ad154a02be491c363b965745b3
nj <nj@example.com>
2017-12-20 16:30:39 +0800

    修改程序图标和名称。

--- app/src/main/java/com/example/administrator/coolweather/service/AutoUpdateService.java
+++ app/src/main/java/com/example/administrator/coolweather/service/AutoUpdateService.java
@@ -50,7 +50,7 @@ public class AutoUpdateService extends Service {
             // 有缓存时直接解析天气数据
             Weather weather = Utility.handleWeatherResponse(weatherString);
             String weatherId = weather.basic.weatherId;
-            String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=bc0418b57b2d4918819d3974ac1285d9";
+            String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=e0affa8c4eaf4a89af0a4dc2d8de53b2";
             HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
                 @Override
                 public void onResponse(Call call, Response response) throws IOException {
